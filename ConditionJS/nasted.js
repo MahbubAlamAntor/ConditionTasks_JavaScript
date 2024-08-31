a0a5a99af64b5913a081b23e3c0af5bfcdaa5096
Mahbub8340 <mdmahbubalam573125@gmail.com>
2024-08-31 08:13:46 +0600

    nasted conditon practice

---
+++ ConditionJS/nasted.js
@@ -0,0 +1,27 @@
+const hand = -1;
+if (hand>=300){
+    console.log('rich');
+}
+else{
+    if(hand>=200){
+        console.log('medium');
+    }
+    else{
+        if(hand>100){
+            console.log('cholar')
+        }
+        else {
+            if(hand>50){
+                console.log('tar por o ai');
+            }
+            else{
+                if(hand>0){
+                    console.log('asar doder nei')
+                }
+                else{
+                    console.log('tui amar bondhu na');
+                }
+            }
+        }
+    }
+}
